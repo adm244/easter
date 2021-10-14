9b3dec604f01792b92f64877db024cd10e0c1e86
Adam Worgen <adm2444@gmail.com>
2021-10-14 11:47:01 +0300

    Implemented PGF file writer

--- Easter/Program.cs
+++ Easter/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
-using System.Drawing;
+using System.Runtime.InteropServices;
 
 using EasterLib;
-using SixLabors.ImageSharp;
+
 using K4os.Compression.LZ4;
-using System.Diagnostics;
+
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace Easter
@@ -22,15 +24,25 @@ namespace Easter
                 Package.Unpack(archives[i]);
             }*/
 
-            using (FileStream stream = new FileStream(args[0], FileMode.Open, FileAccess.Read))
+            //PGFDecode(args[0], args[0] + ".png");
+            //PGFEncode(args[0], args[0] + ".pgf");
+
+            //NOTE(adm244): packed json is just a "message pack" thing:
+            // https://github.com/msgpack/msgpack/blob/master/spec.md
+        }
+
+        private static byte[] _pgfMagic = new byte[] { 0x50, 0x47, 0x46 };
+
+        public static void PGFDecode(string source, string target)
+        {
+            using (FileStream stream = new FileStream(source, FileMode.Open, FileAccess.Read))
             {
                 using (BinaryReader reader = new BinaryReader(stream))
                 {
-                    UInt32 value = reader.ReadUInt32();
-                    UInt32 magic = value & 0x00FFFFFF;
-                    byte unk08Size = (byte)(value >> 24);
+                    byte[] magic = reader.ReadBytes(_pgfMagic.Length);
+                    Debug.Assert(magic == _pgfMagic);
 
-                    Debug.Assert(magic == 0x00464750);
+                    byte unk08Size = reader.ReadByte();
 
                     UInt32 fileSize = reader.ReadUInt32();
 
@@ -58,7 +70,46 @@ namespace Easter
                     Debug.Assert(written > 0);
 
                     Image image = Image.LoadPixelData<Rgba32>(pixels, (int)width, (int)height);
-                    image.SaveAsPng(args[0] + ".png");
+                    image.SaveAsPng(target);
+                }
+            }
+        }
+
+        public static void PGFEncode(string source, string target)
+        {
+            Image<Rgba32> image = Image.Load<Rgba32>(source);
+
+            image.TryGetSinglePixelSpan(out var span);
+            byte[] pixels = MemoryMarshal.AsBytes(span).ToArray();
+            byte[] pixelsCompressed = new byte[pixels.Length + (int)(pixels.Length * 0.04 + 0.5)];
+
+            int compressedSize = LZ4Codec.Encode(pixels, pixelsCompressed);
+
+            using (FileStream stream = new FileStream(target, FileMode.Create, FileAccess.Write))
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    writer.Write(_pgfMagic);
+
+                    writer.Write((byte)0x0);
+
+                    writer.Write((UInt32)0xDEADBEEF);
+                    writer.Write((UInt32)compressedSize);
+
+                    writer.Write((UInt32)image.Width);
+                    writer.Write((UInt32)image.Height);
+
+                    writer.Write((UInt16)0x44A2);
+
+                    writer.Write((byte)0x0);
+
+                    writer.Write((byte)0x0);
+
+                    writer.Write(pixelsCompressed, 0, compressedSize);
+
+                    UInt32 fileSize = (UInt32)writer.BaseStream.Position;
+                    writer.BaseStream.Seek(0x4, SeekOrigin.Begin);
+                    writer.Write((UInt32)fileSize);
                 }
             }
         }
